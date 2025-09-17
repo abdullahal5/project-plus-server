b08f044ccd281a5841a7f2efdd0c5994ece3fda9
abdullahal5 <abdullahalfahin183@gmail.com>
2025-09-17 19:14:52 +0600

    created graphql auth guard and fixed railway build problem

--- src/Project/project.resolver.ts
+++ src/Project/project.resolver.ts
@@ -1,17 +1,15 @@
-import { Query, Resolver, Info } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { Project } from 'src/graphql/entities/models.entities';
 import { ProjectService } from './project.service';
-import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { UseGuards } from '@nestjs/common';
-import { PrismaSelect } from '@paljs/plugins';
-import type { GraphQLResolveInfo } from 'graphql';
+import { GqlAuthGuard } from 'src/guards/gql-guard';
 
 @Resolver(() => Project)
 export class ProjectResolver {
   constructor(private readonly projectService: ProjectService) {}
 
   @Query(() => [Project])
-  //   @UseGuards(JwtAuthGuard)
+  @UseGuards(GqlAuthGuard)
   async projects(): Promise<Project[]> {
     return this.projectService.getAllProjects();
   }
