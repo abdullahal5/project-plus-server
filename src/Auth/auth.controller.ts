2d38e0c781d5803a177a053d07933edb6d11360a
abdullahal5 <abdullahalfahin183@gmail.com>
2025-09-17 14:27:15 +0600

    refresh token route is created

--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { sendResponse } from 'src/common/utilities';
 import { AuthServices } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
-import type { Response } from 'express';
+import type { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -29,6 +37,26 @@ export class AuthController {
         accessToken,
         role,
       },
+    });
+  }
+
+  @Post('/refresh-token')
+  async refreshToken(@Req() req: Request, @Res() res: Response) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const refreshToken = req.cookies['refresh_token'];
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token not found');
+    }
+
+    const { accessToken, role } =
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      await this.authService.refreshAccessToken(refreshToken);
+
+    return sendResponse(res, {
+      statusCode: HttpStatus.OK,
+      success: true,
+      message: 'Access token refreshed successfully',
+      data: { accessToken, role },
     });
   }
 }
--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from 'src/Prisma/prisma.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import bcrypt from 'bcrypt';
 import { JwtUtil } from 'src/common/utilities';
+import { JsonWebTokenError, TokenExpiredError } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthServices {
@@ -42,5 +43,38 @@ export class AuthServices {
     const refreshToken = this.jwtUtil.generateToken(payload, '7d');
 
     return { accessToken, refreshToken, role: payload.role };
+  }
+
+  async refreshAccessToken(refreshToken: string) {
+    try {
+      const payload = this.jwtUtil.verifyToken(refreshToken);
+
+      const user = await this.prisma.user.findUnique({
+        where: { id: payload.id },
+      });
+      if (!user)
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+
+      const accessToken = this.jwtUtil.generateToken(
+        { id: user.id, email: user.email, role: user.role },
+        '1h',
+      );
+
+      return { accessToken, role: user.role };
+    } catch (error) {
+      if (error instanceof TokenExpiredError)
+        throw new HttpException(
+          'Refresh token expired',
+          HttpStatus.UNAUTHORIZED,
+        );
+
+      if (error instanceof JsonWebTokenError)
+        throw new HttpException(
+          'Invalid refresh token',
+          HttpStatus.UNAUTHORIZED,
+        );
+
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+    }
   }
 }
